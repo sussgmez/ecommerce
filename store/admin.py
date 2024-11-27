00df9b49ea630c0e6a7a8c0f99a6e612b90090a0
Jesus Gomez <jesusgdea@gmail.com>
2024-11-27 08:51:27 -0400

    add project

---
+++ store/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Product, ProductImage, Order, Category
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
---
+++ store/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from tinymce import models as tinymodels
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
+"""
+TODO:
+> Estudiar el sistema de inventario.
+""" 
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(_("Nombre categoria"), max_length=50)
+
+
+class Product(models.Model):
+    name = models.CharField(_("Nombre"), max_length=200)
+    description = tinymodels.HTMLField(_('Descripcion'))
+    category = models.ManyToManyField(Category, verbose_name=_("Categorias"))
+    stock = models.IntegerField(_("Stock"), default=0)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, verbose_name=_(""), on_delete=models.CASCADE)
+    image = models.ImageField(_("Imagen"), upload_to='Products/') 
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("Usuario"), on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name=_("Producto"), on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(_("Cantidad"))
+
+
+"""
+@receiver(pre_save, sender=Order)
+def order_pre_save_receiver(sender, instance, **kwargs):
+    if instance.product.stock < 0:
+        pass
+"""
+
+
